98e863fa07950d2d3a8b78b4684312ae940d4349
sagarstha254 <sagarstha254@gmail.com>
2022-02-27 16:54:21 +0545

    change text in HTML

---
+++ index.js
@@ -0,0 +1,11 @@
+var a =3;
+var b = 17;
+var c =12;
+
+/*a = a +12;
+b = b + 9;
+c += 6;*/
+console.log(a);
+console.log(b);
+console.log(c);
+console.log("Hello world");
---
+++ p1.js
@@ -0,0 +1,4 @@
+function callit()
+{
+    document.getElementById("demon").innerHTML="Well Done! You have successfully changed the paragraph"
+}
